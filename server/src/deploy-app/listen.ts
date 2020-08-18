889e722d7f4d1f722005bf8e0aa9745fed44f8e1
sleep-written <sleep.written0000@gmail.com>
2020-08-18 09:01:52 -0400

    Minor changes in listen deploy step

--- server/src/deploy-app/listen.ts
+++ server/src/deploy-app/listen.ts
@@ -3,10 +3,8 @@ import { appConfig } from '.';
 import { Log } from '../tool/log';
 
 export const listen: Interface.Action = app => {
-    return new Promise((resolve, reject) => {
-        const server = appConfig.data.server;
-        app.listen(server.port, () => {
-            Log.ok('Webservice is deployed, listening...');
-        });
+    const server = appConfig.data.server;
+    app.listen(server.port, () => {
+        Log.ok('Webservice is deployed, listening...');
     });
 };
